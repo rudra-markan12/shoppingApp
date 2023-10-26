77249b506a085780a506dcff728316e501ef1be5
rudra-markan12 <rudramarkan0@gmail.com>
2023-10-26 17:11:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ALERT PROMT 
+// documetn.querySelector(".className #idName")
+// if else
+// for
+// hw confirm
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
